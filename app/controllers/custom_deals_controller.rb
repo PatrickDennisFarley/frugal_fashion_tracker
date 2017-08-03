4d35055e1e1e938f8b2ff54e0dccb0aadfb81573
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-03 14:23:28 -0400

    added react API endpoints & deployed react front-end

--- app/controllers/custom_deals_controller.rb
+++ app/controllers/custom_deals_controller.rb
@@ -4,25 +4,6 @@ require 'pry'
 
 class CustomDealsController < ApplicationController
   def index
-    session = Redd.it(
-      user_agent: 'Redd:FashionTexter:v1.0.0 (by /u/the4oclockhero)',
-      client_id:  'ylDT9JmKnj76CA',
-      secret:     'IWeJkCkF821w05gekSpUg0CPtaM',
-      username:   'the4oclockhero',
-      password:   'hv3kk444'
-    )
-
-    @keywords = ["J.Crew"]
-
-    @deals = []
-    @keywords.each do |keyword|
-      sales = session.subreddit('frugalmalefashion').search(keyword, sort: :new)
-      sales = sales.sort_by { |sale| sale.created }.reverse
-      sales.each do |sale|
-        if sale.link_flair_text == "[Deal/Sale]"
-          @deals << sale
-        end
-      end
-    end
+    render template: 'static_pages/index'
   end
 end
--- app/controllers/custom_discussions_controller.rb
+++ app/controllers/custom_discussions_controller.rb
@@ -4,24 +4,6 @@ require 'pry'
 
 class CustomDiscussionsController < ApplicationController
   def index
-    session = Redd.it(
-      user_agent: 'Redd:FashionTexter:v1.0.0 (by /u/the4oclockhero)',
-      client_id:  'ylDT9JmKnj76CA',
-      secret:     'IWeJkCkF821w05gekSpUg0CPtaM',
-      username:   'the4oclockhero',
-      password:   'hv3kk444'
-    )
-
-    @keywords = ["Finish Line", "J.Crew", "Uniqlo"]
-
-    @discussions = []
-    @keywords.each do |keyword|
-      sales = session.subreddit('frugalmalefashion').search(keyword, sort: :new)
-      sales.each do |sale|
-        if sale.link_flair_text == "[Discussion]"
-          @discussions << sale
-        end
-      end
-    end
+    render template: 'static_pages/index'
   end
 end
