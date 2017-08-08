07a0122b6a27d60204324e85c81adbc7dfad35d9
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-08 11:09:37 -0400

    add readme file

---
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -0,0 +1,15 @@
+# require "rails_helper"
+#
+# RSpec.describe Api::V1::PostsController, type: :controller do
+#   let!(:first_post) { Post.create(name: "Hello this is Dog", url: "https://media.giphy.com/media/pSpmpxFxFwDpC/giphy.gif", likes: 25) }
+#   let!(:second_post) = { Post.create(name: "Pug Swimming", url: "https://media.giphy.com/media/r6ALgGVKLg93O/giphy.gif", likes: 47) }
+#
+#   describe "GET#all_posts" do
+#     it "should return a list of all the posts" do
+#
+#       get :index
+#       returned_json = JSON.parse(response.body)
+#
+#     end
+#   end
+# end
