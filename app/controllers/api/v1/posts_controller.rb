68ff3e447fa21f70cd78e9b72f1cc894de114aaa
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-10 15:23:52 -0400

    Update puts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,6 +2,8 @@ require 'redd'
 
 class Api::V1::PostsController < ApplicationController
   def all_posts
+    puts (ENV)
+    puts (ENV["USER_AGENT"])
 
     session = Redd.it(
        user_agent: ENV["USER_AGENT"],
